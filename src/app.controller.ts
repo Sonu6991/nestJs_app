ddfc592b8c4c452a96aa3bd1c0ff1be3452ef650
Sonu6991 <sonuchauhan8488@gmail.com>
2024-03-11 18:21:22 +0530

    microservice

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get("/getResut")
+  @Get()
+  @MessagePattern({ cmd: 'getHello' })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import { TcpOptions, Transport } from '@nestjs/microservices';
 dotenv.config();
 const port = process.env.PORT;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: +port,
+      host: 'localhost',
+    },
+  } as TcpOptions);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -17,6 +25,6 @@ async function bootstrap() {
       },
     }),
   );
-  await app.listen(port);
+  await app.listen();
 }
 bootstrap();
